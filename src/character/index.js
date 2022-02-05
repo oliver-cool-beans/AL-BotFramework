c6b52ca8f32a7d2620515a793904e0ae9a77de74
olivertribe <oliver.lh.tribe@gmail.com>
2022-02-05 13:06:56 +1100

    Update sellitems

--- src/character/index.js
+++ src/character/index.js
@@ -49,7 +49,7 @@ class Character {
         this.tasks = [];
         this.serverRegion = "US", 
         this.serverIdentifier = "I"
-        this.itemsToSell = [{name: "hpbelt", level: 0}, {name: "hpamulet", level: 0}] // TODO put this in dynamic config accessable by discord
+        this.itemsToSell = [{name: "hpbelt", level: 0}, {name: "hpamulet", level: 0}, {name: "vitscroll"}, {name: "mushroomstaff", level: 0}] // TODO put this in dynamic config accessable by discord
     }
 
     async start(AL) {
