b9bc26af22466ab01574d83d31971572d40cbf73
olivertribe <oliver.lh.tribe@gmail.com>
2022-02-09 22:27:38 +1100

    Add tiger to squig

--- src/scripts/squig/index.js
+++ src/scripts/squig/index.js
@@ -6,7 +6,7 @@
 
 import utils from "../../scripts/utils/index.js";
 
-const targets = ["Phoenix", "Squig"];
+const targets = ["Phoenix", "Tiger", "Squig", "Squigtoad"];
 
 async function squig(bot, party, merchant, args) {
     if(!bot.character.ready) return Promise.reject("Character not ready");
